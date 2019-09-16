ac165c771a1a2ee32b63b9c9fc5cea5a7c5335d4
chenBright <1021774709@qq.com>
2019-09-17 00:05:16 +0800

    Opt：完善noncopyable (#25)

--- base/noncopyable.h
+++ base/noncopyable.h
@@ -5,7 +5,9 @@ namespace tinyWS {
     // 不可拷贝类
     class noncopyable {
     protected:
-        // 默认的构造函数和析构函数是 protected
+        // 默认的构造函数和析构函数是 protected，
+        // 不允许创建 noncopyable 实例，但允许子类创建实例
+        // （即允许派生类构造和析构）。
         noncopyable() = default;
 
         ~noncopyable() = default;
@@ -14,7 +16,7 @@ namespace tinyWS {
         // 使用 delete 关键字禁止编译器自动产生复制构造函数和复制赋值操作符。
         noncopyable(const noncopyable&) = delete;
 
-        const noncopyable& operator=(const noncopyable&) = delete;
+        noncopyable& operator=(const noncopyable&) = delete;
     };
 }
 
